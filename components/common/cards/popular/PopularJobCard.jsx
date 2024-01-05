19b7e1c11dcac60ca2d4e806265d4aa6e16e60cd
internet_trash <naiknischal92@gmail.com>
2024-01-06 00:36:13 +0530

    finish popular jobs

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,25 +1,43 @@
 import * as react  from 'react';
 import { View, Text , TouchableOpacity, Image } from 'react-native';
+//import {checkImageURL} from '../../../../utils';
 
 import styles from './popularjobcard.style'
 
-const PopularJobCard = ({item, selectedJob,handlejobcardpress}) => {
+const checkImageURL = (url) => {
+  return url && url.trim() !== "";
+};
+
+const PopularJobCard = ({item, selectedJob, handlejobcardpress}) => {
   return (
-   <TouchableOpacity
-    style={styles.container(selectedJob,item)}
-    onPress= {()=>handlejobcardpress(item)}
-   >
-   <TouchableOpacity style={styles.logoContainer(selectedJob,item)}>
-    <Image
-    source={{ uri: item.employer_logo }}
-    resizeMode="contain" 
-    style={styles.logoImage}
-    />
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handlejobcardpress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
 
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      <View>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>
+          {item.job_country}
+        </Text>
+      </View>
     </TouchableOpacity>
-  </TouchableOpacity>
+  );
+};
 
-  )
-}
+export default PopularJobCard;
 
-export default PopularJobCard
